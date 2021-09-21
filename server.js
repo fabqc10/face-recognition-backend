c2a197c2578dd94495e936e9acf35fa3704f449d
Fabio <faqc10@gmail.com>
2021-09-21 15:44:11 +0200

    adding smart-brain-api

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+
+const db = knex({
+  
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '1234',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res)=> { res.send(db.users) })
+
+app.post('/signin', (req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+
+app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id', (req,res)=>{profile.handleProfileGet(req,res,db)})
+
+app.put('/image', (req,res)=>{image.handleImage(req,res,db)});
+
+app.post('/imageurl', (req,res)=>{image.handleApiCall(req,res)});
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=> {
+  console.log(`app is running on port ${PORT}`);
+})
